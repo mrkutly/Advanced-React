64eeb1b87dde29b7233f49975e590d0c5232be29
mark sauer-utley <mark.utley.713@gmail.com>
2019-03-17 17:24:29 -0400

    graphql setup and yoga server creation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const mutations = {};
+const Mutations = {
+	async createItem(parent, args, ctx, info) {
+		// create the item
+		// args are the properties of the object we are creating, so we can just spread them
+		// info contains the actual query that got passed in, so we know what to return after the item is created
+		const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
+		return item;
+	},
+};
 
-module.exports = mutations;
+module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,14 @@
-const Query = {};
+// if you are just doing simple things that do not require any auth or special actions,
+// you can use prisma's "forwardTo" func to send the query directly to your DB.
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+	items: forwardTo("db"),
+	// this is the same as...
+	// ? async items(parent, args, ctx, info) {
+	// ?	const items = await ctx.db.query.items();
+	// ?	return items;
+	// ? },
+};
 
 module.exports = Query;
